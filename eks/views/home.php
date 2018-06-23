9d970597e9dd42edccd52910b7f33fceed19b155
jamesnagle <james.rnagle@gmail.com>
2018-06-23 17:05:54 -0400

    refactored icon roll over effects

--- eks/views/home.php
+++ eks/views/home.php
@@ -6,15 +6,15 @@ Template::load('header.php'); ?>
 <div class="row center">
     <div class="block flex header">
         <div>
-            <a href="" title="DL that Résumé"><i class="fas fa-chevron-up marker"></i><i class="fas fa-file-pdf"></i></a>
-            <a href="" title="Mah GitHub"><i class="fas fa-chevron-up marker"></i><i class="fab fa-github-alt"></i></a>
-            <a href="" title="I can has GitLab"><i class="fas fa-chevron-up marker"></i><i class="fab fa-gitlab"></i></a>
+            <a href="" title="DL that Résumé"><i class="fas fa-file-pdf"></i></a>
+            <a href="" title="Mah GitHub"><i class="fab fa-github-alt"></i></a>
+            <a href="" title="I can has GitLab"><i class="fab fa-gitlab"></i></a>
         </div>
         <h1 class="h1__title"><span class="mright">James</span><span class="mleft">Nagle</span></h1>
         <div>
-            <a href="" title="Dat Insta tho"><i class="fas fa-chevron-up marker"></i><i class="fab fa-instagram"></i></a>
-            <a href="" title="Twittaa"><i class="fas fa-chevron-up marker"></i><i class="fab fa-twitter"></i></a>
-            <a href="" title="Been YouTube'in, since been YouTube'in"><i class="fas fa-chevron-up marker"></i><i class="fab fa-youtube"></i></a>
+            <a href="" title="Dat Insta tho"><i class="fab fa-instagram"></i></a>
+            <a href="" title="Twittaa"><i class="fab fa-twitter"></i></a>
+            <a href="" title="Been YouTube'in, since been YouTube'in"><i class="fab fa-youtube"></i></a>
         </div>
     </div>
 </div>
